3ba4c6c4753a3a40f49957c9b9237ba52aae4568
407-29-Chawin <54654@yupparaj.ac.th>
2026-07-24 11:36:13 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,11 +1,9 @@
-import streamlit as st
+ import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-vat = price * 0.07
 net_price = price - vat
+vat = price * 0.07
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
-
 st.divider()
-st.write("นายชวิน  ไชยศรี เลขที่ 29  ม.4/7")
+st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
