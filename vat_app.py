5fba263ee85f2b6d68fbdb38ad66fe3515cb42c3
407-29-Chawin <54654@yupparaj.ac.th>
2026-07-24 11:32:44 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,9 +3,9 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-net_price = price - vat
 vat = price * 0.07
+net_price = price - vat
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+st.write("นายชวิน  ไชยศรี เลขที่ 29  ม.4/7")
